a1275b60c2dc55b334418c5fc3afeaee05056b03
nehsa25 <jesse.stone@nehsa.net>
2024-04-06 16:47:05 -0700

    updated docker-compose

--- nehsa.net/src/app/main/gcp/gcp.component.ts
+++ nehsa.net/src/app/main/gcp/gcp.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-gcp',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './gcp.component.html',
   styleUrl: './gcp.component.scss'
 })
