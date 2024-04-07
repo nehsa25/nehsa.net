d63d35119d0f38164111ae2b2835f7bd5a4c0fbd
nehsa25 <jesse.stone@nehsa.net>
2024-04-07 13:55:38 -0700

    fixed playwright gcp test

--- tests/pages/gcp.page.ts
+++ tests/pages/gcp.page.ts
@@ -4,7 +4,7 @@ export default class GcpPage extends BasePage {
     name: string = "gcp-page";
     pathstem: string = 'gcp';
     _title = 'Jesse Stone - nehsa.net';
-    get headerTitle(): string { return "Google Cloud Platform" }
+    get headerTitle(): string { return "Google Cloud Platform (gcp)" }
     get title(): string { return this._title }    
     get path(): string { return `${this.settings.APP_ENVIRONMENT}/${this.pathstem}`; };
 
