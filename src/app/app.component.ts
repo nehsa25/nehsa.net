16dd63150e32b2bfef8639714e5a27068bd2cd62
nehsa25 <jesse.stone@nehsa.net>
2024-04-08 12:50:48 -0700

    Added breadcrum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared-components/navbar/navbar.component';
 import { CornerListenerComponent } from './shared-components/corner-listener/corner-listener.component';
+import { MatIcon } from '@angular/material/icon';
+import { BreadcrumbComponent, BreadcrumbItemDirective } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CornerListenerComponent],
+  imports: [RouterOutlet, NavbarComponent, CornerListenerComponent, MatIcon,  BreadcrumbComponent, BreadcrumbItemDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- tests/playwright-tests.spec.ts
+++ tests/playwright-tests.spec.ts
@@ -1,14 +1,16 @@
 import { test, expect } from '@playwright/test';
 import { TestSettings } from './utility/test-settings';
+import { routes } from '../src/app/app.routes';
+import { Route } from '@angular/router';
 
-let testSettings:TestSettings = new TestSettings(null);
+let testSettings: TestSettings = new TestSettings(null);
 
 /** A paramaterized function which checks each page has the correct page title */
 for (const pageDefinition of testSettings.PAGE_DEFINITIONS) {
   test(`nehsa.net: Page \"${pageDefinition.name}\" has correct title`, async ({ page }) => {
     let testpage = new pageDefinition(page, testSettings);
     await testpage.goto();
-    await expect(page).toHaveTitle(testpage.title, {timeout: testSettings.defaultTimeoutMs}); 
+    await expect(page).toHaveTitle(testpage.title, { timeout: testSettings.defaultTimeoutMs });
   });
 }
 
@@ -22,14 +24,19 @@ for (const pageDefinition of testSettings.PAGE_DEFINITIONS) {
 }
 
 /** Confirms each page renders in x time */
-
 for (const duration of testSettings.PAGE_LOAD_DURATIONS_SECS) {
   for (const pageDefinition of testSettings.PAGE_DEFINITIONS) {
     test(`nehsa.net: Page \"${pageDefinition.name}\" renders in ${duration} `, async ({ page }) => {
       let testpage = new pageDefinition(page, testSettings);
       await testpage.goto();
-      await expect(page).toHaveTitle(testpage.title, {timeout: duration * 1000}); 
+      await expect(page).toHaveTitle(testpage.title, { timeout: duration * 1000 });
     });
   }
 }
 
+/** Confirms all pages are being tested */
+test(`nehsa.net: Confirm all pages are being tested`, async ({ page }) => {
+  for (const testpage in testSettings.PAGE_DEFINITIONS) {
+    console.log(testpage);
+  }    
+});
