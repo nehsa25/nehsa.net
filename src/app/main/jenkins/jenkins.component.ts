c2fe2a8e50bea2d8fd04879bb4d2c6f4da431fc3
nehsa25 <jesse.stone@nehsa.net>
2024-04-17 14:32:12 -0700

    Added section for Jenkins

---
+++ src/app/main/jenkins/jenkins.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-jenkins',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './jenkins.component.html',
+  styleUrl: './jenkins.component.scss'
+})
+export class JenkinsComponent {
+
+}
