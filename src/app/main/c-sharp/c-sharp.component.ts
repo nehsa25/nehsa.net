3b685f23ca328d4774a6ee715c2ef2765dcf0255
nehsa25 <jesse.stone@nehsa.net>
2024-05-22 09:35:08 -0700

    Added content for software development/c#

--- src/app/main/c-sharp/c-sharp.component.ts
+++ src/app/main/c-sharp/c-sharp.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-c-sharp',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatIcon],
   templateUrl: './c-sharp.component.html',
   styleUrl: './c-sharp.component.scss'
 })
