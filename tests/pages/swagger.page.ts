dbc58b687023c83dd9a5fafa43e3639247247373
nehsa25 <jesse.stone@nehsa.net>
2024-05-24 07:53:47 -0700

    Fixed broken swagger playwright test

--- tests/pages/swagger.page.ts
+++ tests/pages/swagger.page.ts
@@ -4,7 +4,7 @@ export default class SwaggerPage extends BasePage {
     override name: string = "swagger-page";
     pathstem: string = '#/swagger';
     private _headertitle: string = "Swagger";
-    private _title = 'nehsa.net | Swagger';
+    private _title = 'nehsa.net | Swagger Documentation';
     get headerTitle(): string { return this._headertitle }
     get title(): string { return this._title }    
     get path(): string { return `${this.settings.APP_ENVIRONMENT}/${this.pathstem}`; };
