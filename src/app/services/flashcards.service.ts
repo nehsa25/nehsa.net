ee31840f3de9cece103e60463e98ebac0b38eb09
nehsa25 <jesse.stone@nehsa.net>
2024-06-12 16:49:16 -0700

    Improved Flashcard service

--- src/app/services/flashcards.service.ts
+++ src/app/services/flashcards.service.ts
@@ -5,12 +5,24 @@ import { Question } from '../types/question.type';
 export class FlashcardsService {
     Questions: Question[] = [];
     QuestionAnswer: any = null;
+    ViewedQuestions: Question[] = [];
+    current = 1;
+
     constructor() { }
     public setQuestion() {
+
         this.QuestionAnswer = this.Questions[Math.floor(Math.random() * this.Questions.length)];
+        while (this.ViewedQuestions.includes(this.QuestionAnswer)) {
+            this.QuestionAnswer = this.Questions[Math.floor(Math.random() * this.Questions.length)];
+        }
+        this.ViewedQuestions.push(this.QuestionAnswer);
+        this.current++;
         return this.QuestionAnswer;
     }
     public setQuestions(questions: Question[]) {
         this.Questions = questions;
+    }
+    public getTotal() {
+        return this.Questions.length + 1;
     }
 }
