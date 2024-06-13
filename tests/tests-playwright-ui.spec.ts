f8443c6e263475d8affe1d4c22a29cfa91bb3b94
nehsa25 <jesse.stone@nehsa.net>
2024-06-13 05:06:31 -0700

    Removed unused test

--- tests/tests-playwright-ui.spec.ts
+++ tests/tests-playwright-ui.spec.ts
@@ -38,18 +38,3 @@ for (const duration of testSettings.PAGE_LOAD_DURATIONS_SECS) {
 test(`nehsa.net: Confirm all pages are being tested`, async ({ page }) => {
   expect (testSettings.PAGE_DEFINITIONS.length-3 == routes.length);
 });
-
-// /** Confirms we can fill out contact us form */
-// test(`nehsa.net: Can submit contact us form`, async ({ page }) => {
-//   // start on home page
-//   const aboutPage = new AboutPage(page, testSettings);
-//   await aboutPage.goto(aboutPage.path);
-
-//   // expact contact us
-//   await aboutPage.contactusExpandButton.click();
-
-//   // fill out form
-//   await aboutPage.contactusSubject.fill("test subject");
-//   await aboutPage.contactusBody.fill("test body");
-//   await aboutPage.contactusButton.click();
-// });
