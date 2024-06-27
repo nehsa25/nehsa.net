0d7c84a37fccb789b3776269f884f20492764e00
nehsa25 <jesse.stone@nehsa.net>
2024-06-27 06:20:43 -0700

    Merged CSS/web design content

--- src/app/main/web-design/web-design.component.ts
+++ src/app/main/web-design/web-design.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommentComponent } from '../../shared-components/comment/comment.component';
 import { MatCardImage, MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-web-design',
   standalone: true,
-  imports: [MatCardModule, CommentComponent, MatExpansionModule, MatCardImage],
+  imports: [MatCardModule, CommentComponent, MatExpansionModule, MatCardImage, MatIcon],
   templateUrl: './web-design.component.html',
   styleUrl: './web-design.component.scss'
 })
