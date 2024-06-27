12301862746c248402eeb62082950978bdcad3c1
nehsa25 <jesse.stone@nehsa.net>
2024-06-27 06:29:13 -0700

    Cleaned up CSS section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -33,7 +33,6 @@ import { DotnetcoreComponent } from './main/dotnetcore/dotnetcore.component';
 import { SwaggerComponent } from './main/swagger/swagger.component';
 import { DesignpatternsComponent } from './main/designpatterns/designpatterns.component';
 import { SignupComponent } from './main/signup/signup.component';
-import { CssComponent } from './main/css/css.component';
 import { GliderComponent } from './main/glider/glider.component';
 import { JavaComponent } from './main/java/java.component';
 import { SqlComponent } from './main/sql/sql.component';
@@ -69,7 +68,6 @@ export const routes: Routes = [
     { path: 'npm', title: 'nehsa.net | npm', component: NpmComponent },
     { path: 'playwright', title: 'nehsa.net | Playwright', component: PlaywrightComponent },
     { path: 'doxy', title: 'nehsa.net | Doxygen', component: DoxygenComponent },
-    { path: 'css', title: 'nehsa.net | Cascading Style Sheets', component: CssComponent },
     { path: 'dot', title: 'nehsa.net | Dot / Graphwiz / Pydot', component: DotComponent },
     { path: 'ohai', title: 'nehsa.net | Ohai', component: OhaiComponent },
     { path: 'powershell', title: 'nehsa.net | Powershell', component: PowershellComponent },
