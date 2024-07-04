028220b12142379d56266a4be3f41c63e683fc69
nehsa25 <jesse.stone@nehsa.net>
2024-07-04 08:19:06 -0700

    Added more Python content

--- src/app/main/python/python.component.ts
+++ src/app/main/python/python.component.ts
@@ -5,11 +5,13 @@ import { Subject } from 'rxjs';
 import { UserService } from '../../services/user.service';
 import { CommentType } from '../../types/comment.type';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-python',
   standalone: true,
-  imports: [MatCardModule, CommentComponent, MatIconModule],
+  imports: [MatCardModule, CommentComponent, MatIconModule, MatButtonModule, NgIf],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './python.component.html',
   styleUrl: './python.component.scss'
@@ -17,12 +19,18 @@ import { MatIconModule } from '@angular/material/icon';
 export class PythonComponent {
   private _page_name = "python";
   totalItems = 0;
+  private _answers: string[] = ["Fucking Jerry."]
+  public displayAnswerOne = "";
 
   constructor(
     public userService: UserService
-  ) {}
+  ) { }
 
-  ngOnInit() { 
+  ngOnInit() {
     this.userService.page = this._page_name;
+  }
+
+  answer(entry: number) {
+    this.displayAnswerOne = this._answers[entry];
   }
 }
