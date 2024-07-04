39382c31d460acf88a0b8f3d6acf8f64762e79dc
nehsa25 <jesse.stone@nehsa.net>
2024-07-04 11:31:36 -0700

    More Python content

--- src/app/main/python/python.component.ts
+++ src/app/main/python/python.component.ts
@@ -1,17 +1,16 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { CommentComponent } from '../../shared-components/comment/comment.component';
-import { Subject } from 'rxjs';
 import { UserService } from '../../services/user.service';
-import { CommentType } from '../../types/comment.type';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { NgIf } from '@angular/common';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-python',
   standalone: true,
-  imports: [MatCardModule, CommentComponent, MatIconModule, MatButtonModule, NgIf],
+  imports: [CommentComponent, MatExpansionModule, MatCardModule, MatIconModule, MatButtonModule, NgIf],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './python.component.html',
   styleUrl: './python.component.scss'
@@ -19,8 +18,8 @@ import { NgIf } from '@angular/common';
 export class PythonComponent {
   private _page_name = "python";
   totalItems = 0;
-  private _answers: string[] = ["Fucking Jerry."]
-  public displayAnswerOne = "";
+  public answerOne = "";
+  public answerTwo = "";
 
   constructor(
     public userService: UserService
@@ -30,7 +29,13 @@ export class PythonComponent {
     this.userService.page = this._page_name;
   }
 
-  answer(entry: number) {
-    this.displayAnswerOne = this._answers[entry];
+  clickAnswerOne() {
+    let answer = "Fucking Jerry."
+    this.answerOne == "" ? this.answerOne = answer : this.answerOne = "";
+  }
+
+  clickAnswerTwo() {
+    let answer = "The print statement would only show the Guard initialization: I'm being constructed baby!"
+    this.answerTwo == "" ? this.answerTwo = answer : this.answerTwo = "";
   }
 }
