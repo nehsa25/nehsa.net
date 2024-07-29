fa39a04c10a2c50d724c2b2765890b9ae797bfbe
nehsa25 <jesse.stone@nehsa.net>
2024-07-29 10:16:16 -0700

    Updated to always scroll to top of page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -111,6 +111,14 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
   }
 
+  onActivate(event: any) {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
   sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const version = { number: '2024-07-29 07:54' }
+export const version = { number: '2024-07-29 10:15' }
