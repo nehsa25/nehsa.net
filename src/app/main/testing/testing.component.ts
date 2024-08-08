1f85db4a20ba2404a4dbdf7da31e82663cbdae7b
nehsa25 <jesse.stone@nehsa.net>
2024-08-08 12:09:52 -0700

    Added section on testing

--- src/app/main/testing/testing.component.ts
+++ src/app/main/testing/testing.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { CommentComponent } from '../../shared-components/comment/comment.component';
 import { MatCardModule } from '@angular/material/card';
 import { UserService } from '../../services/user.service';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-testing',
   standalone: true,
-  imports: [MatCardModule, CommentComponent],
+  imports: [MatCardModule, CommentComponent, MatButtonModule, RouterLink, MatExpansionModule],
   templateUrl: './testing.component.html',
   styleUrl: './testing.component.scss'
 })
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const version = { number: '2024-08-07 16:56' }
+export const version = { number: '2024-08-08 09:32' }
