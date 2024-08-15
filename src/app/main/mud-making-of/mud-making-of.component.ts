566c33f3d85e48b28ee5063603c776815844c0cf
nehsa25 <jesse.stone@nehsa.net>
2024-08-14 17:38:46 -0700

    More making of content

--- src/app/main/mud-making-of/mud-making-of.component.ts
+++ src/app/main/mud-making-of/mud-making-of.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommentComponent } from '../../shared-components/comment/comment.component';
 import { MatCardModule } from '@angular/material/card';
 import { UserService } from '../../services/user.service';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-mud-making-of',
   standalone: true,
-  imports: [MatCardModule, CommentComponent],
+  imports: [MatCardModule, CommentComponent, MatExpansionModule, RouterLink],
   templateUrl: './mud-making-of.component.html',
   styleUrl: './mud-making-of.component.scss'
 })
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const version = { number: '2024-08-13 16:19' }
+export const version = { number: '2024-08-14 17:33' }
