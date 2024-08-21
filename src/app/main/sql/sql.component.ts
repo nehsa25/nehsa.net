3f377a3b572091e027ce1c869b797c1a9713d600
nehsa25 <jesse.stone@nehsa.net>
2024-08-20 21:53:10 -0700

    Updated SQL section

--- src/app/main/sql/sql.component.ts
+++ src/app/main/sql/sql.component.ts
@@ -4,11 +4,12 @@ import { CommentComponent } from '../../shared-components/comment/comment.compon
 import { Subject } from 'rxjs';
 import { UserService } from '../../services/user.service';
 import { CommentType } from '../../types/comment.type';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-sql',
   standalone: true,
-  imports: [MatCardModule, CommentComponent],
+  imports: [MatCardModule, CommentComponent, MatExpansionModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './sql.component.html',
   styleUrl: './sql.component.scss'
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const version = { number: '2024-08-20 15:48' }
+export const version = { number: '2024-08-20 20:55' }
