31a0cca998e31a83b1fd02081bf6ca4499645bc1
nehsa25 <jesse.stone@nehsa.net>
2024-08-26 14:12:29 -0700

    Improved graphql and aws

--- src/app/main/aws/aws.component.ts
+++ src/app/main/aws/aws.component.ts
@@ -5,11 +5,12 @@ import { UserService } from '../../services/user.service';
 import { AsyncPipe, NgIf } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-aws',
   standalone: true,
-  imports: [MatCardModule, CommentComponent, NgIf, MatButtonModule, AsyncPipe, RouterLink],
+  imports: [MatCardModule, CommentComponent, NgIf, MatButtonModule, AsyncPipe, RouterLink, MatExpansionModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './aws.component.html',
   styleUrl: './aws.component.scss'
--- src/app/main/graphql/graphql.component.ts
+++ src/app/main/graphql/graphql.component.ts
@@ -4,13 +4,15 @@ import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { CommentComponent } from '../../shared-components/comment/comment.component';
 import { UserService } from '../../services/user.service';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-graphql',
   standalone: true,
   imports: [MatCardModule, CommonModule, MatExpansionModule, CommentComponent],
   templateUrl: './graphql.component.html',
-  styleUrl: './graphql.component.scss'
+  styleUrl: './graphql.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class GraphqlComponent {
   constructor(public userService: UserService) { this.userService.page = 'GraphQL'; }
