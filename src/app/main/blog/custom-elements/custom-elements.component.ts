b4fd690de5d3a902718f79f02d98a9c7b73042e4
nehsa25 <jesse.stone@nehsa.net>
2024-09-01 07:04:54 -0700

    Added new blog article

---
+++ src/app/main/blog/custom-elements/custom-elements.component.ts
@@ -0,0 +1,21 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { CommentComponent } from '../../../shared-components/comment/comment.component';
+import { MatCardModule } from '@angular/material/card';
+import { UserService } from '../../../services/user.service';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-custom-elements',
+  standalone: true,
+  imports: [MatCardModule, MatExpansionModule, CommentComponent, MatIconModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './custom-elements.component.html',
+  styleUrl: './custom-elements.component.scss'
+})
+export class CustomElementsComponent {
+  constructor(public userService: UserService) {
+    this.userService.page = "Blog - custom elements";
+   }
+
+}
