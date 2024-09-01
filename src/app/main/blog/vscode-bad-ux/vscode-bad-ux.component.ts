706f64b2c796cc1c0162c580db1479986b642eeb
nehsa25 <jesse.stone@nehsa.net>
2024-09-01 09:04:19 -0700

    vscode blog post

--- src/app/main/blog/vscode-bad-ux/vscode-bad-ux.component.ts
+++ src/app/main/blog/vscode-bad-ux/vscode-bad-ux.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommentComponent } from '../../../shared-components/comment/comment.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-vscode-bad-ux',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatExpansionModule, CommentComponent],
   templateUrl: './vscode-bad-ux.component.html',
   styleUrl: './vscode-bad-ux.component.scss'
 })
 export class VscodeBadUxComponent {
+  constructor(public userService: UserService) {
+    this.userService.page = 'Blog - VSCode Bad UX';
+  }
 
 }
