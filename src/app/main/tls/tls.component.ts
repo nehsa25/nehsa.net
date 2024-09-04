a768826639e1e8603da20f47aa8f1503bb22bb42
nehsa25 <jesse.stone@nehsa.net>
2024-09-03 22:12:50 -0700

    Improved TLS content

--- src/app/main/tls/tls.component.ts
+++ src/app/main/tls/tls.component.ts
@@ -4,11 +4,12 @@ import { CommentComponent } from '../../shared-components/comment/comment.compon
 import { Subject } from 'rxjs';
 import { UserService } from '../../services/user.service';
 import { CommentType } from '../../types/comment.type';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-tls',
   standalone: true,
-  imports: [MatCardModule, CommentComponent],
+  imports: [MatCardModule, CommentComponent, MatExpansionModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './tls.component.html',
   styleUrl: './tls.component.scss'
