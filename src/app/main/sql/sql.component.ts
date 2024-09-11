d63ed0b1bc8c6724fc29efcd83086235c719322a
nehsa25 <jesse.stone@nehsa.net>
2024-09-10 22:18:54 -0700

    Fixed sql not showing icons

--- src/app/main/sql/sql.component.ts
+++ src/app/main/sql/sql.component.ts
@@ -5,11 +5,13 @@ import { Subject } from 'rxjs';
 import { UserService } from '../../services/user.service';
 import { CommentType } from '../../types/comment.type';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sql',
   standalone: true,
-  imports: [MatCardModule, CommentComponent, MatExpansionModule],
+  imports: [MatCardModule, CommentComponent, MatExpansionModule, MatIconModule, RouterLink],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './sql.component.html',
   styleUrl: './sql.component.scss'
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const version = { number: '2024-09-10 16:23' }
+export const version = { number: '2024-09-10 22:11' }
