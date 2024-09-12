44a1cf76b9cc23789a33e677ceefa69c722bd591
Jesse Stone <jesse.stone@nehsa.net>
2024-09-12 16:35:41 -0700

    Updated content

--- src/app/main/linux/linux.component.ts
+++ src/app/main/linux/linux.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MatCard, MatCardModule } from '@angular/material/card';
 import { CommentComponent } from '../../shared-components/comment/comment.component';
 import { UserService } from '../../services/user.service';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-linux',
   standalone: true,
-  imports: [MatCardModule, CommentComponent],
+  imports: [MatCardModule, CommentComponent, MatExpansionModule, RouterLink],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './linux.component.html',
   styleUrl: './linux.component.scss'
 })
