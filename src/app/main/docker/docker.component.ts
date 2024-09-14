71d8e55291adb5a925c7883beb2d14f7b95ff97c
jesse.stone@nehsa.net <jesse.stone@nehsa.net>
2024-09-14 08:54:32 -0700

    Improved docker content

--- src/app/main/docker/docker.component.ts
+++ src/app/main/docker/docker.component.ts
@@ -5,11 +5,12 @@ import { UserService } from '../../services/user.service';
 import { CommentType } from '../../types/comment.type';
 import { Subject } from 'rxjs';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-docker',
   standalone: true,
-  imports: [MatCardModule, CommentComponent, MatExpansionModule],
+  imports: [MatCardModule, CommentComponent, MatExpansionModule, MatIcon],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './docker.component.html',
   styleUrl: './docker.component.scss'
