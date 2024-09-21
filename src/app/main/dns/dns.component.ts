f2f78ca936bcef9810e881b10c7f6abf40559408
jesse.stone@nehsa.net <jesse.stone@nehsa.net>
2024-09-21 11:29:52 -0700

    Updated DNS section

--- src/app/main/dns/dns.component.ts
+++ src/app/main/dns/dns.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { RouterLink } from '@angular/router';
@@ -12,6 +12,7 @@ import { RelatedContentComponent } from '../../shared-components/related-content
   selector: 'app-dns',
   standalone: true,
   imports: [MatCardModule, CommonModule, MatExpansionModule, RouterLink, MatButtonModule, CommentComponent, RelatedContentComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './dns.component.html',
   styleUrl: './dns.component.scss'
 })
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const version = { number: '2024-09-20 10:28' }
+export const version = { number: '2024-09-21 11:22' }
